d204e638cad86d0fb1d9c565e319dcf48d8e9190
apeden <apeden23@gmail.com>
2019-02-24 23:20:23 +0000

    Uploading source files for the FacePamphlet Assignment (Stanford Uni Java course), an excercise in creating software for a social network

---
+++ FacePamphlet.java
@@ -0,0 +1,164 @@
+/* 
+ * File: FacePamphlet.java
+ * -----------------------
+ * When it is finished, this program will implement a basic social network
+ * management system.
+ */
+
+import acm.program.*;
+import acm.graphics.*;
+import acm.util.*;
+
+import java.awt.Canvas;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
+
+public class FacePamphlet extends Program 
+					implements FacePamphletConstants {
+
+	/**
+	 * This method has the responsibility for initializing the 
+	 * interactors in the application, and taking care of any other 
+	 * initialization that needs to be performed.
+	 */
+	public void init() {
+		profileDatabase = new FacePamphletDatabase();
+		add(profileCanvas);
+		nameField = new JTextField(TEXT_FIELD_SIZE);
+		statusField = new JTextField(TEXT_FIELD_SIZE);
+		statusField.addActionListener(this);
+		pictureField = new JTextField(TEXT_FIELD_SIZE);
+		pictureField.addActionListener(this);
+		friendField = new JTextField(TEXT_FIELD_SIZE);
+		friendField.addActionListener(this);
+		add(new JLabel("Name"), NORTH);
+		add(nameField, NORTH);
+		add(new JButton("Add"), NORTH);
+		add(new JButton("Delete"), NORTH);
+		add(new JButton("Lookup"), NORTH);
+		add(statusField, WEST);
+		add(new JButton("Change Status"), WEST);
+		add(new JLabel(EMPTY_LABEL_TEXT), WEST);
+		add(pictureField, WEST);
+		add(new JButton("Change Picture"), WEST);
+		add(new JLabel(EMPTY_LABEL_TEXT), WEST);
+		add(friendField, WEST);
+		add(new JButton("Add Friend"), WEST);
+		// You fill this in
+		addActionListeners();
+    }
+   
+  
+    /**
+     * This class is responsible for detecting when the buttons are
+     * clicked or interactors are used, so you will have to add code
+     * to respond to these actions.
+     */
+    public void actionPerformed(ActionEvent e) {
+    	String cmd = e.getActionCommand();
+		if ((cmd.equals("Add")) && (checkJTextField(nameField))) {
+			String name = nameField.getText();
+			if (profileDatabase.containsProfile(name)) {
+				currentProfile = profileDatabase.getProfile(name);
+				messageString = "A profile already exists for " + name;		
+			}else {		
+				FacePamphletProfile newProfile = new FacePamphletProfile(name);
+				profileDatabase.addProfile(newProfile);
+				currentProfile = newProfile;
+				messageString = "";
+			}
+			
+		}else if ((cmd.equals("Delete")) && (checkJTextField(nameField))) {
+			currentProfile = null;
+			String name = nameField.getText();
+			if (profileDatabase.containsProfile(name)) {
+				profileDatabase.deleteProfile(name);
+				currentProfile = null;
+				messageString = "Profile for " + name + " was deleted.";			
+			}
+			
+		}else if ((cmd.equals("Lookup")) && (checkJTextField(nameField))) {
+			String name = nameField.getText();
+			if (profileDatabase.containsProfile(name)) {
+				FacePamphletProfile profile = profileDatabase.getProfile(name);
+				currentProfile = profile;
+				messageString = "";
+				
+			} else {
+				currentProfile = null;
+				messageString = "The profile for " + name + " does not exist.";
+			}
+
+		}else if (currentProfile == null) {
+			messageString = "Please choose a profile in 'name' before adding a status, "
+					+ "picture or friend.";
+			
+    	}else if ((cmd.equals("Change Status"))||(e.getSource() == statusField)) {
+    		if (checkJTextField(statusField)) { 			
+    			currentProfile.setStatus(statusField.getText());
+    			messageString = "";
+    		}
+    		
+		}else if ((cmd.equals("Change Picture"))||(e.getSource() == pictureField)) {
+    		if  (checkJTextField(pictureField)) {
+    			GImage image = null;
+    			try {
+    				image = new GImage(pictureField.getText());
+    				currentProfile.setImage(image);
+    				messageString = "";
+    				
+    			}catch (ErrorException ex) {
+    				messageString = "Sorry, we cannot find and image with this name";
+    			}// Code that is executed if the filename cannot be opened.
+    		}
+    		
+		}else if ((cmd.equals("Add Friend"))||(e.getSource() == friendField)) {
+			if (friendField.getText().equalsIgnoreCase(currentProfile.getName())){
+					messageString = "You can't add yourself as a friend!!";
+			}else if  (checkJTextField(friendField)) {
+				if (profileDatabase.containsProfile(friendField.getText())) {
+					currentProfile.addFriend(friendField.getText());
+					messageString = "";
+					profileDatabase.getProfile(friendField.getText()).addFriend(currentProfile.getName());
+				}else {
+					messageString = "A profile does not yet exist for " + friendField.getText();
+				}
+				
+			}	
+		}profileCanvas.displayProfile(currentProfile);
+		profileCanvas.showMessage(messageString);
+		displayAllPersons(profileDatabase.getProfileMap());
+		
+	}
+			
+	public boolean checkJTextField(JTextField field) {
+		String regex = "[a-zA-Z0-9]+";
+		for (int i = 0; i < field.getText().length(); i ++) {
+			if (field.getText().substring(i, i+1).matches(regex)) {
+				return true;
+			}
+		}return false;
+	}
+	
+	public void displayAllPersons(HashMap <String, FacePamphletProfile> profileDatabaseMap) {
+		Iterator<String> profileDatabaseMapIt = profileDatabaseMap.keySet().iterator();
+		println("display all persons called");
+		print (profileDatabaseMapIt.hasNext());
+		while (profileDatabaseMapIt.hasNext()) {	
+			println(profileDatabaseMapIt.next().toString());
+		}
+	}
+    	
+    	// You fill this in as well as add any additional methods
+    /*private instance variables*/
+    JTextField nameField;
+    JTextField statusField;
+    JTextField pictureField;
+    JTextField friendField;
+    String messageString = "";
+    FacePamphletProfile currentProfile;
+    FacePamphletDatabase profileDatabase;
+    FacePamphletCanvas profileCanvas = new FacePamphletCanvas();
+	
+}
---
+++ FacePamphletCanvas.java
@@ -0,0 +1,142 @@
+/*
+ * File: FacePamphletCanvas.java
+ * -----------------------------
+ * This class represents the canvas on which the profiles in the social
+ * network are displayed.  NOTE: This class does NOT need to update the
+ * display when the window is resized.
+ */
+
+
+import acm.graphics.*;
+import java.awt.*;
+import java.util.*;
+
+public class FacePamphletCanvas extends GCanvas 
+					implements FacePamphletConstants {
+
+
+	/** 
+	 * Constructor
+	 * This method takes care of any initialization needed for 
+	 * the display
+	 */
+	public FacePamphletCanvas() {
+		// You fill this in
+	}
+
+	
+	/** 
+	 * This method displays a message string near the bottom of the 
+	 * canvas.  Every time this method is called, the previously 
+	 * displayed message (if any) is replaced by the new message text 
+	 * passed in.
+	 */
+	public void showMessage(String msg) {	
+		if (message != null) {
+			remove(message);
+		}message = new GLabel (msg);
+		message.setFont(MESSAGE_FONT);
+		message.setLocation((APPLICATION_WIDTH-message.getWidth())/2, 
+							APPLICATION_HEIGHT-BOTTOM_MESSAGE_MARGIN);
+		add(message);
+	}
+		
+	
+	/** 
+	 * This method displays the given profile on the canvas.  The 
+	 * canvas is first cleared of all existing items (including 
+	 * messages displayed near the bottom of the screen) and then the 
+	 * given profile is displayed.  The profile display includes the 
+	 * name of the user from the profile, the corresponding image 
+	 * (or an indication that an image does not exist), the status of
+	 * the user, and a list of the user's friends in the social network.
+	 */
+	public void displayProfile(FacePamphletProfile profile) {
+		removeAll();
+		if (profile != null) {
+			addName(profile.getName());
+			if (profile.getImage() != null) {
+				addImage(profile.getImage());
+			}else {
+				addEmptyRec();
+			}addStatus(profile.getStatus());
+			addFriends(profile.getFriends());
+		}
+	}		
+	
+	private void addEmptyRec() {
+		add(new GRect(LEFT_MARGIN, TOP_MARGIN + IMAGE_MARGIN, 
+										IMAGE_WIDTH, 
+										IMAGE_HEIGHT));
+		addLabel("No Image", 
+					PROFILE_IMAGE_FONT, 
+					LEFT_MARGIN + (IMAGE_WIDTH/2), 
+					TOP_MARGIN + (IMAGE_HEIGHT/2),
+					true,
+					Color.black);
+	}
+			
+	private void addStatus(String status) {		
+		String statusForDisplay = "No current status";
+		if (!status.isEmpty()) {
+			statusForDisplay = status;
+		}addLabel(statusForDisplay, 
+			PROFILE_STATUS_FONT, 
+			LEFT_MARGIN, 
+			TOP_MARGIN + IMAGE_MARGIN + IMAGE_HEIGHT + STATUS_MARGIN,
+			false,
+			Color.black);
+	}
+	
+	private void addName(String name) {
+		addLabel(name, 
+				PROFILE_NAME_FONT, 
+				LEFT_MARGIN, 
+				TOP_MARGIN,
+				false,
+				Color.blue);
+	}	
+	
+	private void addImage(GImage image) {
+		image.setLocation(LEFT_MARGIN, TOP_MARGIN + IMAGE_MARGIN);
+		image.setSize(IMAGE_WIDTH, IMAGE_HEIGHT);
+		add(image);	
+	}	
+	
+	private void addFriends(Iterator<String> getFriendsIt) {
+		addLabel("Friends:", 
+				PROFILE_FRIEND_LABEL_FONT, 
+				APPLICATION_WIDTH/2, 
+				TOP_MARGIN, 
+				false, 
+				Color.black);
+		double count = 1.0;
+		while (getFriendsIt.hasNext()){ 
+			String friend = getFriendsIt.next();
+			GLabel friendLabel = new GLabel(friend); 
+			friendLabel.setFont(PROFILE_FRIEND_FONT);
+			friendLabel.setLocation(APPLICATION_WIDTH/2, 
+									TOP_MARGIN + (count * friendLabel.getHeight()));
+			count += 1.0;
+			add(friendLabel);
+		}
+	}		
+		
+		
+	private void addLabel(String label, String font, 
+			double xleftLocation, double ytopLocation, boolean centre, Color color) {
+		GLabel newLabel = new GLabel (label);
+		newLabel.setFont(font);
+		newLabel.setColor(color);
+		if (centre) {
+			newLabel.setLocation(xleftLocation - newLabel.getWidth()/2, ytopLocation + newLabel.getHeight()/2);
+		} else  
+			newLabel.setLocation(xleftLocation, ytopLocation);	
+		add(newLabel);
+	}
+	
+	
+	/*private instance variables*/
+	GLabel message;
+}
+
